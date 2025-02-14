5d4cc94a0bea5f73d279176e572649ec02070e84
= <=>
2025-02-14 10:08:32 +0100

    Github exception patch

--- src/main/java/org/example/tiendaonline/Excepciones/ExcepcionesPersonalizadasNoEncontrado.java
+++
@@ -1,7 +0,0 @@
-package org.example.tiendaonline.Excepciones;
-
-public class ExcepcionesPersonalizadasNoEncontrado extends RuntimeException{
-    public ExcepcionesPersonalizadasNoEncontrado(String message) {
-        super(message);
-    }
-}
